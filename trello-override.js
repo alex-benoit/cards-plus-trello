bfba2fbaab2d59db80a6bb615e9374c1d0b92732
Alex Benoit <alexander.benoit@outlook.com>
2017-02-19 12:28:47 +0000

    script on document idle

--- trello-override.js
+++ trello-override.js
@@ -1,7 +1,6 @@
-var numberCards = $('.list-card-title').length
-$('.board-header-btns.mod-left').append('<a id="total-cards" class="board-header-btn" href="#" title="Total Cards">Total Cards: ' + numberCards + '</a>')
-
-$('#total-cards').click(function() {
-  var numberCards = $('.list-card-title').length
-  $(this).text('Total Cards: ' + numberCards)
+$(function() {
+  var numberCards = $('.list-card', '.list').size()
+  $('.board-header-btns.mod-left').append('<a id="total-cards" class="board-header-btn" href="#" title="Total Cards">Total Cards: ' + numberCards + '</a>')
 });
+
+
