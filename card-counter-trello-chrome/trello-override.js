247a40ef4ee02957a3aa7ca1079f978af17919ca
Alex Benoit <alex@lewagon.com>
2017-11-22 20:10:16 +0000

    udpated removed jquery

--- card-counter-trello-chrome/trello-override.js
+++ card-counter-trello-chrome/trello-override.js
@@ -1,10 +1,21 @@
-function calculateCardCount() {
-  var numberCards = $('.list-card').size();
-  if ($('#total-cards').size() > 0) {
-    $('#total-cards-count').text(numberCards);
+function checkOnBoard() {
+  if (window.location.href.includes('trello.com/b/')) {
+    return true
   } else {
-    $('.board-header-btns.mod-left').append('<div id="total-cards">Total Cards: <span id="total-cards-count">' + numberCards + '</span></div>');
+    return false
   }
 }
 
-setInterval(calculateCardCount, 800);
+function calculateCardCount() {
+  if (checkOnBoard()) {
+    var numberCards = document.querySelectorAll('.list-card').length;
+    var counterElement = document.getElementById('total-cards')
+    if (counterElement == null) {
+      document.querySelector('.board-header-btns.mod-left').insertAdjacentHTML('beforeend', '<div id="total-cards">Total Cards: <span id="total-cards-count">' + numberCards + '</span></div>')
+    } else {
+      document.getElementById('total-cards-count').innerText = numberCards;
+    }
+  }
+}
+
+setInterval(calculateCardCount, 750);
